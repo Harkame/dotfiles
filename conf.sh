c2cce3e0eee2256a485d48ff472da68bf5c9e5b0
Louis <louisr.daviaud@gmail.com>
2016-07-01 01:46:54 +0200

    Create conf.sh

---
+++ conf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /home/harkame/.config/i3
+mv .config/i3/config /home/harkame/.config/i3/config
+
+mv .Xdefaults /home/harkame/.Xdefaults
+
+mv ./etc/pacman.conf /etc/pacman.conf
