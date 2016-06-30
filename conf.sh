5ac10d9868c948dd536445efb354adc2a514d9eb
Louis <louisr.daviaud@gmail.com>
2016-07-01 01:53:23 +0200

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p /home/harkame/.config/i3
-mv .config/i3/config /home/harkame/.config/i3/config
+mv ../.config/i3/config /home/$USER/.config/i3/config
 
-mv .Xdefaults /home/harkame/.Xdefaults
+mv ../.Xdefaults /home/$USER/.Xdefaults
 
-mv ./etc/pacman.conf /etc/pacman.conf
+mv ../etc/pacman.conf /etc/pacman.conf
