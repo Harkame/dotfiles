93e751d759db4a6ec00e4213278225fb5f25d1ac
Louis <louisr.daviaud@gmail.com>
2018-03-03 12:20:20 +0100

    Create package.sh

---
+++ install/package.sh
@@ -0,0 +1,82 @@
+yaourt -S
+	xorg-server
+	xorg-xinit 
+	xorg-xmessage 
+	xorg-utils 
+	xorg-xrandr
+
+	rxvt-unicode 
+
+	i3
+	i3-gaps
+	i3blocks
+
+	firefox-i18n-fr
+	flashplugin
+
+	git
+
+	sunflower
+
+	lxtask
+
+	vte
+	ntfs-3g
+	boost-libs
+	lib32-libldap
+
+	vlc
+	mplayer
+
+	feh
+
+	gksu
+	wine
+	rtmpdump
+	neofetch
+	playonlinux
+	lxappearance
+
+	eclipse-java
+		2) jdk-8
+
+	zip
+	unzip
+	rar
+	P7zip
+
+	atom
+
+	tor
+
+	megasync
+
+	calibre
+
+	alsa-utils
+
+	gnu-cobol
+
+	zsh
+
+	gpmdp-remote
+
+	gdb
+
+	transmission-cli
+	qbittorrent
+
+	gparted
+	btrfs-progs
+
+	oh-my-zh-git
+	wireshark-cli
+	cutegram-git
+	discorde
+	telegram-cli-git
+	telegram-desktop
+	texlive-core
+	lm_sensors
+	ttf-ms-fonts
+
+	speedtest-cli
