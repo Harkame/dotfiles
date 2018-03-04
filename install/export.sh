c191ea22e49d8be2d1a24d4e451d8faa5abd40f2
harkame <louisr.daviaud@gmail.com>
2018-03-05 00:16:50 +0100

    Fix

--- install/export.sh
+++ install/export.sh
@@ -4,7 +4,7 @@ cp -r -f  ../dotfiles/.config /home/$USER/;
 
 cp -r -f  ../dotfiles/.oh-my-zsh /home/$USER/;
 
-cp -r -f  ../dotfiles/.ascii_script /home/$USER/;
+cp -r -f  ../dotfiles/ascii_script /home/$USER/;
 
 cp -r -f  ../dotfiles/.Xdefaults /home/$USER/;
 
