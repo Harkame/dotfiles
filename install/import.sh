ceb74c59c67a37af62d20dd0c45dc7ff7c85b20f
harkame <louisr.daviaud@gmail.com>
2018-09-09 04:43:28 +0200

    clear

--- install/import.sh
+++ install/import.sh
@@ -4,7 +4,11 @@ mkdir -p ../dotfiles/etc/;
 
 cp -r -f /home/$USER/.xinitrc ../dotfiles/;
 
-cp -r -f /home/$USER/.config/ ../dotfiles/;
+cp -r -f /home/$USER/.config/i3 ../dotfiles/.config/i3;
+
+cp -r -f /home/$USER/.config/i3blocks ../dotfiles/.config/i3blocks;
+
+cp -r -f /home/$USER/.config/neofetch ../dotfiles/.config/neofetch;
 
 cp -r -f /home/$USER/.oh-my-zsh/ ../dotfiles/;
 
