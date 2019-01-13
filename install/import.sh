4b468385903eebeadab1cc392e62382d571447e9
Harkame <louisr.daviaud@gmail.com>
2019-01-13 14:19:57 +0100

    fix

--- install/import.sh
+++ install/import.sh
@@ -4,15 +4,15 @@ mkdir -p ../dotfiles/etc/;
 
 mkdir -p ../dotfiles/.local/share/fonts/;
 
-cp -p -f /home/$USER/.local/share/fonts ../dotfiles/.local/share/fonts;
+cp -r -f /home/$USER/.local/share/fonts ../dotfiles/.local/share/fonts;
 
 cp -r -f /home/$USER/.xinitrc ../dotfiles/;
 
-cp -r -f /home/$USER/.config/i3 ../dotfiles/.config/i3;
+cp -r -f /home/$USER/.config/i3 ../dotfiles/.config/;
 
-cp -r -f /home/$USER/.config/i3blocks ../dotfiles/.config/i3blocks;
+cp -r -f /home/$USER/.config/i3blocks ../dotfiles/.config/;
 
-cp -r -f /home/$USER/.config/neofetch ../dotfiles/.config/neofetch;
+cp -r -f /home/$USER/.config/neofetch ../dotfiles/.config/;
 
 cp -r -f /home/$USER/.oh-my-zsh/ ../dotfiles/;
 
---
+++ install/new.sh
@@ -0,0 +1,1 @@
+trizen -Sy --noconfirm xorg-server xorg-xinit xorg-xmessage xorg-apps xorg-xrandr rxvt-unicode i3 i3-gaps i3blocks
