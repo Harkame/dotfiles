d534422d3dacc36331175962fb1179c74eeb52ba
Harkame <louisr.daviaud@gmail.com>
2023-09-18 09:31:13 +0200

    dev

--- install/arch/end_install.sh
+++ install/arch/end_install.sh
@@ -1,39 +1,6 @@
 #!/bin/bash
 
-pacman -Syu yay
-
-yay -Syyu --noconfirm '
-	xorg-server
-	xorg-xini
-	xorg-xmessage
-	xorg-apps
-	xorg-xrandr
-	rxvt-unicode
-	i3
-	i3-gaps
-	i3blocks
-	vivaldi
-	flashplugin
-	sunflower
-	lxtask
-	ntfs-3g
-	boost-libs
-	libldap
-	mplayer
-	feh
-	gksu
-	rtmpdump
-	neofetch
-	lxappearance
-	alsa-utils
-	zsh
-	gdb
-	btrfs-progs
-	oh-my-zsh-git
-	lm_sensors
-	ttf-ms-fonts
-	pycharm-comminuty-edition
-	neofetch'
+sudo pacman -Syu yay
 
 echo "exec i3" >> .xinitrc
 
