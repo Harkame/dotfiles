12cf5c0685477174233fcf81eab2c04873c84bb5
Harkame <louisr.daviaud@gmail.com>
2023-09-19 21:59:07 +0200

    Dev

--- install/other/install.sh
+++ install/other/install.sh
@@ -15,25 +15,23 @@ swapon /dev/sda2
 
 mkfs.btrfs -L "Gentoo" -f -n 65536 /dev/sda3
 
-mount --mkdir /dev/sda3 /mnt/gentoo
-mount --mkdir /dev/sda1 /mnt/gentoo/boot
+mount --mkdir /dev/sda3 /mnt
+mount --mkdir /dev/sda1 /mnt/boot
 
-cd /mnt/gentoo
+cd /mnt
 
 wget https://distfiles.gentoo.org/releases/amd64/autobuilds/20230917T164636Z/stage3-amd64-openrc-20230917T164636Z.tar.xz
-
 tar xpf stage*
 
-mount --types proc /proc /mnt/gentoo/proc
-mount --rbind /sys /mnt/gentoo/sys
-mount --make-rslave /mnt/gentoo/sys
-mount --rbind /dev /mnt/gentoo/dev
-mount --make-rslave /mnt/gentoo/dev
-mount --bind /run /mnt/gentoo/run
-mount --make-slave /mnt/gentoo/run
+mount --rbind /dev /mnt/dev
+mount --rbind /proc /mnt/proc
+mount --rbind /sys /mnt/sys
+
 cp /etc/resolv.conf etc
 
 chroot . bash -c '
+  mount /dev/sda3 /
+
   emerge-webrsync
   emerge -DN @world
 
