7ab2d5992eb5d20812a040a78c704958fbf41838
Harkame <louisr.daviaud@gmail.com>
2023-09-20 15:40:57 +0200

    Fix grub-install path

--- install/arch/install.sh
+++ install/arch/install.sh
@@ -58,7 +58,7 @@ arch-chroot /mnt bash -c '
 	then
 		grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB --recheck
 	else
-		grub-install --target=i386-pc --bootloader-id=GRUB --recheck /dev/sda1
+		grub-install --target=i386-pc --bootloader-id=GRUB --recheck /boot
 	fi
 
 	sed -i "s/^BINARIES=()/BINARIES=(setfont)/g" /etc/mkinicpio.conf
