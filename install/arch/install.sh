70f083344602d71ff5b21b20ce5fdb6a6144885d
Harkame <louisr.daviaud@gmail.com>
2024-01-09 02:36:56 +0100

    Update install.sh

--- install/arch/install.sh
+++ install/arch/install.sh
@@ -66,7 +66,7 @@ arch-chroot /mnt bash -c '
 
 	if [ -d "/sys/firmware/efi" ]
 	then
-		grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+		grub-install --target=x86_64-efi --efi-directory=esp --bootloader-id=GRUB
 	else
 		grub-install --target=i386-pc /dev/sda
 	fi
